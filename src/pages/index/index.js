253568ed87a30cde72ce174e19365f06fb12a1bb
Leon <wanliangwang@163.com>
2018-03-08 10:20:24 +0800

    使用mint-ui

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,20 +3,38 @@ import './index.css'
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
+// import { InfiniteScroll } from 'mint-ui'
+// Vue.use(InfiniteScroll)
 
 let app = new Vue({
   el: '#app',
   data: {
-    lists: null
+    lists: null,
+    pageNum: 1,
+    loading: false,
+    allLoaded: false
   },
   created () {
-    axios.post(url.hostLists, {
-      pageNum: 1,
-      pageSize: 6
-    }).then(res => {
-      this.lists = res.data.lists
-      console.log(this.lists)
-    })
+    this.getLists()
+  },
+  methods: {
+    getLists () {
+      this.loading = true
+      axios.post(url.hostLists, {
+        pageNum: this.pageNum,
+        pageSize: 6
+      }).then(res => {
+        let curLists = res.data.lists
+        if (this.lists) {
+          // 将请求的数据合并
+          this.lists = this.lists.concat(curLists)
+        } else {
+          // 初始化,第一次请求数据
+          this.lists = curLists
+        }
+        this.loading = false
+      })
+    }
   }
 })
 

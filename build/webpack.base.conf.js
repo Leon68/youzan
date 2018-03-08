205d1b92192ebd5c4e8277929ec8d4309d926b86
Leon <wanliangwang@163.com>
2018-03-08 09:13:08 +0800

    模拟数据修改最热商品

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,9 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'css': '@/modules/css',
+      'js': '@/modules/js',
+      'components': '@/components',
     }
   },
   module: {

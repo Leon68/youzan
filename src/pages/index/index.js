fb0caed84d9bd7b26ac39cad3aa8b3feecaa3867
Leon <wanliangwang@163.com>
2018-03-08 13:57:10 +0800

    底部组件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,8 +3,10 @@ import './index.css'
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
-// import { InfiniteScroll } from 'mint-ui'
-// Vue.use(InfiniteScroll)
+import { InfiniteScroll } from 'mint-ui'
+Vue.use(InfiniteScroll)
+
+import Foot from 'components/Foot.vue'
 
 let app = new Vue({
   el: '#app',
@@ -19,12 +21,17 @@ let app = new Vue({
   },
   methods: {
     getLists () {
+      // 判断数据是否加载完毕
+      if (this.allLoaded) return
       this.loading = true
       axios.post(url.hostLists, {
         pageNum: this.pageNum,
-        pageSize: 6
+        pageSize: this.pageSize
       }).then(res => {
         let curLists = res.data.lists
+        if (curLists.length < this.pageSize) {
+          this.allLoaded = true
+        }
         if (this.lists) {
           // 将请求的数据合并
           this.lists = this.lists.concat(curLists)
@@ -33,8 +40,12 @@ let app = new Vue({
           this.lists = curLists
         }
         this.loading = false
+        this.pageNum++
       })
     }
+  },
+  components: {
+    Foot
   }
 })
 
